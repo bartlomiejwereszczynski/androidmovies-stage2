ce3791ff7c15032d91e35c2ea5443e75dceb24be
Bartlomiej Wereszczynski <werexx@gmail.com>
2017-02-19 14:05:10 +0100

    Favourite Manager

--- app/src/main/java/com/example/werek/themoviedb/model/FavouriteManager.java
+++ app/src/main/java/com/example/werek/themoviedb/model/FavouriteManager.java
@@ -5,6 +5,7 @@ import android.content.ContentValues;
 import android.content.Context;
 import android.database.Cursor;
 import android.net.Uri;
+import android.util.Log;
 
 import com.example.werek.themoviedb.model.contentprovider.MovieContract.FavouriteEntry;
 import com.example.werek.themoviedb.util.MovieDbApi;
@@ -17,6 +18,7 @@ import java.util.ArrayList;
  */
 
 public class FavouriteManager {
+    public static final String TAG = FavouriteManager.class.getSimpleName();
     private final Context mContext;
 
     public FavouriteManager(Context context) {
@@ -28,9 +30,11 @@ public class FavouriteManager {
         ContentResolver contentResolver = mContext.getContentResolver();
         Movie stored = fetchFavourite(movie.getId());
         if (stored != null) {
-            contentResolver.update(FavouriteEntry.buildEntryUri(movie.getId()),cv,null,null);
+            contentResolver.update(FavouriteEntry.buildEntryUri(movie.getId()), cv, null, null);
+            Log.d(TAG, "storeFavourite: updated movie " + movie);
         } else {
-            contentResolver.insert(FavouriteEntry.CONTENT_URI,cv);
+            contentResolver.insert(FavouriteEntry.CONTENT_URI, cv);
+            Log.d(TAG, "storeFavourite: inserted movie " + movie);
         }
         return this;
     }
@@ -39,7 +43,8 @@ public class FavouriteManager {
         ContentResolver contentResolver = mContext.getContentResolver();
         Movie stored = fetchFavourite(movie.getId());
         if (stored != null) {
-            contentResolver.delete(FavouriteEntry.buildEntryUri(movie.getId()),null,null);
+            contentResolver.delete(FavouriteEntry.buildEntryUri(movie.getId()), null, null);
+            Log.d(TAG, "removeFavourite: deleted movie from db " + movie);
         }
         return this;
     }
@@ -47,7 +52,7 @@ public class FavouriteManager {
     public Movie fetchFavourite(int id) {
         Uri movieUri = FavouriteEntry.buildEntryUri(id);
         Cursor cursor = mContext.getContentResolver().query(movieUri, null, null, null, null);
-        if (cursor.moveToFirst()){
+        if (cursor.moveToFirst()) {
             Movie stored = MovieDbApi.fromSingleCursor(cursor);
             stored.isFavourite = Movie.FAV_YES;
             cursor.close();
@@ -67,7 +72,7 @@ public class FavouriteManager {
 
         Cursor cursor = mContext.getContentResolver().query(FavouriteEntry.CONTENT_URI, null, null, null, null);
         if (cursor.getCount() > 0) {
-            while (cursor.moveToNext()){
+            while (cursor.moveToNext()) {
                 list.getResults().add(MovieDbApi.fromSingleCursor(cursor));
             }
         }
